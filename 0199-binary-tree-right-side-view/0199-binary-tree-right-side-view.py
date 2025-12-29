878df09583ac7d1e8d76ddebb1ce7f3fba64fe6f
saiteja0367 <57909765+saiteja0367@users.noreply.github.com>
2025-12-29 16:13:18 -0500

    Time: 0 ms (100%), Space: 17.5 MB (87.75%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,22 +6,33 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        def bfs(root):
-            if not root:
-                return []
-            result=[]
-            queue=[root]
-            while queue:
-                len_size=len(queue)
-                for i in range(len_size):
-                    node=queue.pop(0)
-                    if i==0:
-                        result.append(node.val)
-                    if node.right:
-                        queue.append(node.right)
-                    if node.left:
-                        queue.append(node.left)
-            return result
-        return bfs(root)
+        # def bfs(root):
+        #     if not root:
+        #         return []
+        #     result=[]
+        #     queue=[root]
+        #     while queue:
+        #         len_size=len(queue)
+        #         for i in range(len_size):
+        #             node=queue.pop(0)
+        #             if i==0:
+        #                 result.append(node.val)
+        #             if node.right:
+        #                 queue.append(node.right)
+        #             if node.left:
+        #                 queue.append(node.left)
+        #     return result
+        # return bfs(root)
+        result = []
+        def dfs(node, level):
+            if not node:
+                return
+            if level == len(result):
+                result.append(node.val)
+            dfs(node.right, level + 1)
+            dfs(node.left, level + 1)
+        dfs(root, 0)
+        return result
+
 
         
