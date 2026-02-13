d6d4bd9c96eabde8b947919f1759b9301c296e3a
saiteja0367 <57909765+saiteja0367@users.noreply.github.com>
2026-02-13 15:39:18 -0500

    Time: 284 ms (86.5%), Space: 19.5 MB (50.94%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        n=len(bombs)
+        graph = {i: [] for i in range(n)}
+        for i in range(n):
+            x1,y1,r1=bombs[i]
+            for j in range(n):
+                if i==j:
+                    continue
+                x2,y2,_=bombs[j]
+                dx=x1-x2
+                dy=y1-y2
+                if dx*dx+dy*dy<=r1*r1:
+                    graph[i].append(j)
+        def bfs(start):
+            visited=set()
+            queue=deque([start])
+            visited.add(start)
+            while queue:
+                node=queue.popleft()
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append(neighbor)
+            return len(visited)
+        max_den=0
+        for i in range(n):
+            max_den=max(max_den,bfs(i))
+        return max_den
+
+
+        
