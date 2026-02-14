a5033bba96d8f3478c4792eef0e7447ad07beff1
saiteja0367 <57909765+saiteja0367@users.noreply.github.com>
2026-02-14 17:21:00 -0500

    Time: 35 ms (34.09%), Space: 44.1 MB (37.92%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        n=len(edges)+1
+        degree=[0]*(n+1)
+        for u,v in edges:
+            degree[u]+=1
+            degree[v]+=1
+        for i in range(1,n+1):
+            if degree[i]==n-1:
+                return i
+        #t.c->o(n)
+        #s.c->o(n)
+        
