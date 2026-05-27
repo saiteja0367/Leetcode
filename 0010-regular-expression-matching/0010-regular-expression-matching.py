fb8aa15fd20572518b371ecf99f4c62a3980d5c8
SaiTeja <57909765+saiteja0367@users.noreply.github.com>
2026-05-27 15:08:24 -0400

    Time: 3 ms (91.34%), Space: 20.6 MB (17.86%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,29 +1,63 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        # memo = {}
+        # def dfs(i, j):
+        #     if (i, j) in memo:
+        #         return memo[(i, j)]
+        #     # both string and pattern finished
+        #     if i >= len(s) and j >= len(p):
+        #         return True
+
+        #     # pattern finished but string not finished
+        #     if j >= len(p):
+        #         return False
+
+        #     # check current character match
+        #     match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+
+        #     # handle '*'
+        #     if j + 1 < len(p) and p[j + 1] == '*':
+        #         # 1) skip x*   OR   2) use x* (if match)
+        #         ans = dfs(i, j + 2) or (match and dfs(i + 1, j))
+        #     else:
+        #         # no '*', must match current char
+        #         ans = match and dfs(i + 1, j + 1)
+
+        #     memo[(i, j)] = ans
+        #     return ans
+
+        # return dfs(0, 0)
         memo = {}
+
         def dfs(i, j):
+            # Check if result is memoized
             if (i, j) in memo:
                 return memo[(i, j)]
-            # both string and pattern finished
-            if i >= len(s) and j >= len(p):
+
+            # If both string and pattern are finished
+            if i == len(s) and j == len(p):
                 return True
 
-            # pattern finished but string not finished
-            if j >= len(p):
+            # If pattern finished but string not
+            if j == len(p):
                 return False
 
-            # check current character match
-            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+            # Check if current characters match
+            first_match = i < len(s) and (p[j] == s[i] or p[j] == '.')
 
-            # handle '*'
+            # Handle '*' operator
             if j + 1 < len(p) and p[j + 1] == '*':
-                # 1) skip x*   OR   2) use x* (if match)
-                ans = dfs(i, j + 2) or (match and dfs(i + 1, j))
+                # Try zero occurrence or one/more occurrences if first_match
+                memo[(i, j)] = (dfs(i, j + 2) or
+                                (first_match and dfs(i + 1, j)))
+                return memo[(i, j)]
             else:
-                # no '*', must match current char
-                ans = match and dfs(i + 1, j + 1)
-
-            memo[(i, j)] = ans
-            return ans
+                # Move both pointers if current characters match
+                if first_match:
+                    memo[(i, j)] = dfs(i + 1, j + 1)
+                    return memo[(i, j)]
+                else:
+                    memo[(i, j)] = False
+                    return False
 
         return dfs(0, 0)
